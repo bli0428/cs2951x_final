82ed66120ca5fd811099967264c522bbeaaefca2
bli0428 <32985874+bli0428@users.noreply.github.com>
2021-12-06 01:43:08 -0500

    Update model.py

--- model/model.py
+++ model/model.py
@@ -86,7 +86,7 @@ def prep_data(data, source_word2idx, target_word2idx, source_words, target_words
     train_samples = len(X_train) # Total Training samples
     val_samples = len(X_test)    # Total validation or test samples
     batch_size = 128
-    epochs = 5
+    epochs = 50
 
     if train == True:
 
