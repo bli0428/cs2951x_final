35d3821f5faf46222414b94aff6111a81e790f4f
Gabriel Rizk <gabrielrizk1@gmail.com>
2021-12-06 15:31:51 -0500

    Fixed TF bug

--- model/model.py
+++ model/model.py
@@ -10,7 +10,10 @@ from keras.layers import Input, LSTM, Embedding, Dense
 from keras.models import Model
 from preprocessing import preprocessing
 import sys
-
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 train = True
 
 def prep_data(data, source_word2idx, target_word2idx, source_words, target_words, rl_idx_to_word):
